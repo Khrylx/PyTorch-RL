5ae09a43dd61c11a472ec9d2997974e2f15fa011
Ye Yuan <khrylx@gmail.com>
2017-11-30 23:12:05 -0500

    small changes to agent structure.

--- core/agent.py
+++ core/agent.py
@@ -6,8 +6,10 @@ import math
 import time
 
 
-def collect_samples(env, memory, policy, custom_reward, tensor, render, running_state, update_rs, min_batch_size):
+def collect_samples(queue, env, policy, custom_reward, mean_action,
+                    tensor, render, running_state, update_rs, min_batch_size):
     log = dict()
+    memory = Memory()
     num_steps = 0
     total_reward = 0
     min_reward = 1e6
@@ -22,11 +24,13 @@ def collect_samples(env, memory, policy, custom_reward, tensor, render, running_
         if running_state is not None:
             state = running_state(state, update=update_rs)
         reward_episode = 0
-        c_reward_episode = 0
 
         for t in range(10000):
             state_var = Variable(tensor(state).unsqueeze(0), volatile=True)
-            action = policy.select_action(state_var)[0].numpy()
+            if mean_action:
+                action = policy(state_var)[0].data[0].numpy()
+            else:
+                action = policy.select_action(state_var)[0].numpy()
             action = int(action) if policy.is_disc_action else action.astype(np.float64)
             next_state, reward, done, _ = env.step(action)
             reward_episode += reward
@@ -35,7 +39,9 @@ def collect_samples(env, memory, policy, custom_reward, tensor, render, running_
 
             if custom_reward is not None:
                 reward = custom_reward(state, action)
-                c_reward_episode += reward
+                total_c_reward += reward
+                min_c_reward = min(min_c_reward, reward)
+                max_c_reward = max(max_c_reward, reward)
 
             mask = 0 if done else 1
 
@@ -54,10 +60,8 @@ def collect_samples(env, memory, policy, custom_reward, tensor, render, running_
         total_reward += reward_episode
         min_reward = min(min_reward, reward_episode)
         max_reward = max(max_reward, reward_episode)
-        total_c_reward += c_reward_episode
-        min_c_reward = min(min_c_reward, c_reward_episode)
-        max_c_reward = max(max_c_reward, c_reward_episode)
 
+    log['num_steps'] = num_steps
     log['num_episodes'] = num_episodes
     log['total_reward'] = total_reward
     log['avg_reward'] = total_reward / num_episodes
@@ -65,56 +69,41 @@ def collect_samples(env, memory, policy, custom_reward, tensor, render, running_
     log['min_reward'] = min_reward
     if custom_reward is not None:
         log['total_c_reward'] = total_c_reward
-        log['avg_c_reward'] = total_c_reward / num_episodes
+        log['avg_c_reward'] = total_c_reward / num_steps
         log['max_c_reward'] = max_c_reward
         log['min_c_reward'] = min_c_reward
 
-    return log
+    if queue is not None:
+        queue.put([memory, log])
+    else:
+        return memory, log
 
 
 def merge_log(log_list):
     log = dict()
     log['total_reward'] = sum([x['total_reward'] for x in log_list])
     log['num_episodes'] = sum([x['num_episodes'] for x in log_list])
+    log['num_steps'] = sum([x['num_steps'] for x in log_list])
     log['avg_reward'] = log['total_reward'] / log['num_episodes']
     log['max_reward'] = max([x['max_reward'] for x in log_list])
     log['min_reward'] = min([x['min_reward'] for x in log_list])
     if 'total_c_reward' in log_list[0]:
         log['total_c_reward'] = sum([x['total_c_reward'] for x in log_list])
-        log['avg_c_reward'] = log['total_c_reward'] / log['num_episodes']
+        log['avg_c_reward'] = log['total_c_reward'] / log['num_steps']
         log['max_c_reward'] = max([x['max_c_reward'] for x in log_list])
         log['min_c_reward'] = min([x['min_c_reward'] for x in log_list])
 
     return log
 
 
-class Worker(multiprocessing.Process):
-
-    def __init__(self, queue, env, policy, custom_reward, tensor, render, running_state, min_batch_size):
-        multiprocessing.Process.__init__(self)
-        self.queue = queue
-        self.env = env
-        self.policy = policy
-        self.custom_reward = custom_reward
-        self.tensor = tensor
-        self.render = render
-        self.running_state = running_state
-        self.min_batch_size = min_batch_size
-
-    def run(self):
-        memory = Memory()
-        log = collect_samples(self.env, memory, self.policy, self.custom_reward, self.tensor,
-                              False, self.running_state, False, self.min_batch_size)
-        self.queue.put([memory, log])
-
-
 class Agent:
 
-    def __init__(self, env_factory, policy, custom_reward=None, render=False,
+    def __init__(self, env_factory, policy, custom_reward=None, mean_action=False, render=False,
                  tensor_type=torch.DoubleTensor, running_state=None, num_threads=1):
         self.env_factory = env_factory
         self.policy = policy
         self.custom_reward = custom_reward
+        self.mean_action = mean_action
         self.running_state = running_state
         self.render = render
         self.tensor = tensor_type
@@ -129,17 +118,17 @@ class Agent:
             self.policy.cpu()
         thread_batch_size = int(math.floor(min_batch_size / self.num_threads))
         queue = multiprocessing.Queue()
-        memory = Memory()
         workers = []
 
         for i in range(self.num_threads-1):
-            workers.append(Worker(queue, self.env_list[i+1], self.policy, self.custom_reward, self.tensor, False,
-                                  self.running_state, thread_batch_size))
+            worker_args = (queue, self.env_list[i + 1], self.policy, self.custom_reward, self.mean_action,
+                           self.tensor, False, self.running_state, False, thread_batch_size)
+            workers.append(multiprocessing.Process(target=collect_samples, args=worker_args))
         for worker in workers:
             worker.start()
 
-        log = collect_samples(self.env_list[0], memory, self.policy, self.custom_reward, self.tensor,
-                              self.render, self.running_state, True, thread_batch_size)
+        memory, log = collect_samples(None, self.env_list[0], self.policy, self.custom_reward, self.mean_action,
+                                      self.tensor, self.render, self.running_state, True, thread_batch_size)
 
         worker_logs = []
         for _ in workers:
@@ -154,4 +143,7 @@ class Agent:
             self.policy.cuda()
         t_end = time.time()
         log['sample_time'] = t_end - t_start
+        log['action_mean'] = np.mean(np.vstack(batch.action), axis=0)
+        log['action_min'] = np.min(np.vstack(batch.action), axis=0)
+        log['action_max'] = np.max(np.vstack(batch.action), axis=0)
         return batch, log
