61960d516c85e912e476b41f764a8a5f8cf38cf8
Ye Yuan <khrylx@gmail.com>
2018-04-02 13:57:18 -0400

    use td-lambda return.

--- core/common.py
+++ core/common.py
@@ -4,21 +4,19 @@ def estimate_advantages(rewards, masks, values, gamma, tau, use_gpu):
     if use_gpu:
         rewards, masks, values = rewards.cpu(), masks.cpu(), values.cpu()
     tensor_type = type(rewards)
-    returns = tensor_type(rewards.size(0), 1)
     deltas = tensor_type(rewards.size(0), 1)
     advantages = tensor_type(rewards.size(0), 1)
 
-    prev_return = 0
     prev_value = 0
     prev_advantage = 0
     for i in reversed(range(rewards.size(0))):
-        returns[i] = rewards[i] + gamma * prev_return * masks[i]
         deltas[i] = rewards[i] + gamma * prev_value * masks[i] - values[i]
         advantages[i] = deltas[i] + gamma * tau * prev_advantage * masks[i]
 
-        prev_return = returns[i, 0]
         prev_value = values[i, 0]
         prev_advantage = advantages[i, 0]
+
+    returns = values + advantages
     advantages = (advantages - advantages.mean()) / advantages.std()
 
     if use_gpu:
